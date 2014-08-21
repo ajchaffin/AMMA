fec4a951fe188880df24aaf86f2ab6125b3b805e
ajchaffin <adamchaffin7@gmail.com>
2014-08-20 22:56:40 -0400

    Added Kris to home, and Instructors page

---
+++ AMMA/Controllers/InstructorsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMMA.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        //
+        // GET: /Instructors/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
