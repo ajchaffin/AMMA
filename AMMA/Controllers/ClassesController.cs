ce2144f6d4ad482a2f503ecc37322b7e356f9cab
ajchaffin <adamchaffin7@gmail.com>
2014-09-01 00:54:23 -0400

    Finished Classes and fixed Schedule

---
+++ AMMA/Controllers/ClassesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMMA.Controllers
+{
+    public class ClassesController : Controller
+    {
+        //
+        // GET: /Classes/
+
+        public ActionResult Index(int id=0)
+        {
+            TempData["CurrentTab"] = id;
+            return View();
+        }
+        
+    }
+}
