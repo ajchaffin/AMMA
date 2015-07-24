7b75e68672ef6f34609d21d6b775c12a5328e0dc
ajchaffin <adamchaffin7@gmail.com>
2015-07-24 11:05:18 -0400

    Added titles to help with search engine optimaization

---
+++ AMMA/Controllers/QuestionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+
+namespace AMMA.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        //
+        // GET: /Questions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitQuestion(AMMA.Models.QuestionsModel model)
+        {
+            var mail = new MailMessage();
+
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+            smtpServer.Credentials = new System.Net.NetworkCredential("adamchaffin7", "Chapstik*3");
+            smtpServer.Port = 587; // Gmail works on this port
+
+            mail.From = new MailAddress(model.EmailAddress);
+            mail.To.Add("adamchaffin7@gmail.com");
+            mail.Subject = "Test";
+            mail.Body = model.Question;
+
+            smtpServer.Send(mail);
+            return View("Index");
+        }
+
+    }
+}
---
+++ AMMA/Models/QuestionsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMMA.Models
+{
+    public class QuestionsModel
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Question is required")]
+        [StringLength(160, ErrorMessage = "{0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        [DataType(DataType.MultilineText)]
+        public string Question { get; set; }
+    }
+}
